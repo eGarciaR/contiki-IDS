a31c0690dcc3a0b726890d4f8ccb6ae9366239dc
eGarciaR <e.garcia.ribera@gmail.com>
2019-05-02 18:22:30 +0100

    fix commit

--- Project/host-ids.c
+++ Project/host-ids.c
@@ -8,7 +8,7 @@
 
 #include "data-sent.h"
 
-#define LOG_MODULE "App"
+#define LOG_MODULE "Node"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 #define WITH_SERVER_REPLY  1
@@ -49,8 +49,6 @@ PROCESS_THREAD(udp_client_process, ev, data)
       LOG_INFO("Sending request %u to ", count);
       LOG_INFO_6ADDR(&dest_ipaddr);
       LOG_INFO_("\n");
-      //snprintf(str, sizeof(str), "Stats");
-      //simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
       
       simple_udp_sendto(&udp_conn, &da_sent, sizeof(da_sent), &dest_ipaddr);
       count++;
