b28aa8a24a21d3394e6fa26ec91e05c8de45b81b
eGarciaR <e.garcia.ribera@gmail.com>
2019-05-08 16:35:39 +0100

    Add Energest monitor to dummy node

--- Project/dummy-node.c
+++ Project/dummy-node.c
@@ -1,26 +1,48 @@
 #include "contiki.h"
 #include <stdio.h> /* For printf() */
 
+#include "sys/energest.h"
+
 /*---------------------------------------------------------------------------*/
-PROCESS(hello_world_process, "Hello world process");
-AUTOSTART_PROCESSES(&hello_world_process);
+PROCESS(dummy_process, "Dummy process");
+AUTOSTART_PROCESSES(&dummy_process);
 /*---------------------------------------------------------------------------*/
 
-PROCESS_THREAD(hello_world_process, ev, data)
+static inline unsigned long
+to_seconds(uint64_t time)
 {
-  static struct etimer timer;
+  return (unsigned long)(time / ENERGEST_SECOND);
+}
+
+PROCESS_THREAD(dummy_process, ev, data)
+{
+  static struct etimer periodic_timer;
 
   PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 60 seconds. */
-  etimer_set(&timer, CLOCK_SECOND * 60);
+  /* Setup a periodic timer that expires after 10 seconds. */
+  etimer_set(&periodic_timer, CLOCK_SECOND * 10);
 
   while(1) {
-    printf("Dummy node is ON!\n");
-
     /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    etimer_reset(&periodic_timer);
+
+    energest_flush();
+
+    printf("\nEnergest:\n");
+    printf(" CPU          %4lus LPM      %4lus DEEP LPM %4lus  Total time %lus\n",
+           to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
+           to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
+           to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
+           to_seconds(ENERGEST_GET_TOTAL_TIME()));
+    printf(" Radio LISTEN %4lus TRANSMIT %4lus OFF      %4lus\n",
+           to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
+           to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+           to_seconds(ENERGEST_GET_TOTAL_TIME()
+                      - energest_type_time(ENERGEST_TYPE_TRANSMIT)
+- energest_type_time(ENERGEST_TYPE_LISTEN)));
+
   }
 
   PROCESS_END();
